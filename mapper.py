556d8ed1d61643c31add0cc1af8dccb33e3bb7fa
kishorvthorat-ux <kishorvthorat@gmail.com>
2026-08-16 17:36:31 +0530

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -19,7 +19,7 @@ def generate_ai_mappings(client: genai.Client, source_schema: dict, target_schem
     """
 
     response = client.models.generate_content(
-        model='gemini-2.5-flash',
+        model='gemini-3.5-flash-lite',
         contents=prompt,
         config=types.GenerateContentConfig(
             response_mime_type="application/json",
